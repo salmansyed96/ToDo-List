e338eb177b0415988366309b94a32fd0248ebae1
salmansyed96 <124539970+salmansyed96@users.noreply.github.com>
2023-07-06 19:12:27 +0530

    add code

--- src/TodoInput.jsx
+++ src/TodoInput.jsx
@@ -1,6 +1,13 @@
 import {useState} from "react";
 
 function TodoInput(props) {
+//   const number=[1,2,3,4,5];
+// const newNuber=number.map(function(number){
+// return <li>{number}</li>
+// });
+// <ul>{newNuber}</ul> 
+
+ 
     const [inputText,setInputText] = useState('');
     const handleEnterPress = (e)=>{
         if(e.keyCode===13){
@@ -8,8 +15,13 @@ function TodoInput(props) {
             setInputText("")
         }
     }
+
+
   return (
+    <>
+    {/* <div><h1>Todo-list</h1></div> */}
     <div className="input-container">
+      
       <input
         type="text"
         className="input-box-todo"
@@ -24,8 +36,12 @@ function TodoInput(props) {
       onClick={()=>{
         props.addList(inputText)
         setInputText("")
-      }}>+</button>      
+      }}>+</button> 
+      {/* <ul>{newNuber}</ul>   */}
+       
     </div>
+    </>
+    
   );
 }
 
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -7,9 +7,13 @@ function Todolist(props) {
         <i className="fa-solid fa-trash-can icon-delete" 
         onClick={e=>{
             props.deleteItem(props.index)
+            
         }}></i>
+       
         </span>
+         
     </li>
+    
   )
 }
 
